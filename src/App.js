440455d81e40b3de29c6eeaf6b0c1f0e493dfcf7
npsk26 <my_nutthapat-26_@hotmail.com>
2019-04-30 22:30:12 +0700

    route pages 😜😜

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter as Router} from "react-router-dom";
+import { RoutePages } from './RoutePages'
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <Router>
+                <RoutePages />
+            </Router>
+        )
+    }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'
+
+// ลิ้งกับ id: app ในไฟล์ index.html และเปิด  App.js  ใน เข้าสู่ index.html
+ReactDOM.render(<App/>,document.getElementById('app'))
